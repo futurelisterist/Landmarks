927e370e9c3d662bda29c154b6e86807dcb057a6
jiseokmin <37jr@naver.com>
2025-06-06 09:22:26 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by SM J on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
