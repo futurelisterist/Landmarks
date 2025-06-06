ef1a0c2984611365fc5915871a2b58f80b836ba5
jiseokmin <37jr@naver.com>
2025-06-06 13:12:29 +0900

    feat: building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by SM J on 6/6/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
