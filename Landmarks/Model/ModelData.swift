6d2ef018262fff31c2d5463c87b5fa3a9e37229a
jiseokmin <37jr@naver.com>
2025-06-15 23:58:02 +0900

    feat: composing complex interfaces

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,21 +7,34 @@
 
 import Foundation
 
+
 @Observable
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+
+
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
-var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
 
